bce9ef88d76363a97f94da843043e7c73a697227
Rian-dev-cod <reyatoa@gmail.com>
2026-02-18 14:59:50 -0300

    First Steps in Java With Spring Boot

---
+++ src/main/java/br/com/rian/eurudio/controllers/GreetingController.java
@@ -0,0 +1,23 @@
+package br.com.rian.eurudio.controllers;
+
+import br.com.rian.eurudio.model.Greeting;
+import lombok.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    // greeting?name=Leandro
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "Word") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template,name));
+    }
+
+}
